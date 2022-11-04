0c18bd9e9e64cd2ad8f65c0d8be63f238abca793
pantonr <philip@p-ant.com>
2022-11-04 15:59:59 -0400

    updates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import win32api
 
 app = Flask(__name__)
 
@@ -7,6 +7,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+
+@app.route('/do_something')
+def do_something():
+  """
+  Do something on button press.
+  """
+  # your code
+  win32api.MessageBox(0, 'hello', 'title')
+  return res
 
 
 
